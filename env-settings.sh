aeeef54c5f72515eb71c5fd9c70d445faecd15d2
Hai-Wei Wang <hwwang2005@gmail.com>
2020-05-03 19:45:00 +0800

    make env enable immediately

--- env-settings.sh
+++ env-settings.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 APP_PATH=$(dirname "$(realpath "$0")")
+BLOON_CLI_HOME=$APP_PATH
 
 rm -f /etc/bash_completion.d/bloon-completion.sh
-ln -s $APP_PATH/etc/bloon-completion.sh /etc/bash_completion.d
+ln -s $BLOON_CLI_HOME/etc/bloon-completion.sh /etc/bash_completion.d
 
 cat << EOF > /etc/profile.d/bloon-path.sh
-export APP_PATH=$APP_PATH
-export PATH=\$APP_PATH/bin:\$PATH
+export BLOON_CLI_HOME=$BLOON_CLI_HOME
+export PATH=\$BLOON_CLI_HOME/bin:\$PATH
 EOF
 
+# --------------------------------------------------
+/etc/profile.d/bloon-path.sh
+/etc/bash_completion.d/bloon-completion.sh
