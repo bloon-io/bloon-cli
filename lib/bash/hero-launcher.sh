d0f81df50ea7b41918cd16b2879b8360a030dbfa
Hai-Wei Wang <hwwang2005@gmail.com>
2020-05-03 20:31:41 +0800

    change launcher

--- lib/bash/hero-launcher.sh
+++ lib/bash/hero-launcher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main:$LD_LIBRARY_PATH
+# for debug only
+# export LD_LIBRARY_PATH=/home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main:$LD_LIBRARY_PATH
+# /home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main/Bloon 2> /dev/null &
 
-/home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main/Bloon 2> /dev/null &
+# TODO use random port
+./bloond 52811 &
 
 sleep 1
