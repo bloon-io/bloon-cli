d9d0eee6acacf8c2311cec3e47c496221c51ff5a
Hai-Wei Wang <hwwang2005@gmail.com>
2020-05-03 22:44:08 +0800

    fix

--- lib/bash/hero-launcher.sh
+++ lib/bash/hero-launcher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 APP_PATH=$(dirname "$(realpath "$0")")
 BLOON_CLI_HOME="$APP_PATH/../.."
+WORK_PATH=$BLOON_CLI_HOME/work
 # --------------------------------------------------
 
 # for debug only
@@ -8,6 +9,6 @@ BLOON_CLI_HOME="$APP_PATH/../.."
 # /home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main/Bloon 2> /dev/null &
 
 # TODO use random port
-$BLOON_CLI_HOME/bloond 52811 &
+$WORK_PATH/BLOON/bloond 52811 &
 
 sleep 1
