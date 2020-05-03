e1121e340153d1430c820d5cd291d128fb32b579
Hai-Wei Wang <hwwang2005@gmail.com>
2020-05-03 20:39:44 +0800

    fix luncher

--- lib/bash/hero-launcher.sh
+++ lib/bash/hero-launcher.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+APP_PATH=$(dirname "$(realpath "$0")")
+BLOON_CLI_HOME="$APP_PATH/../.."
+# --------------------------------------------------
 
 # for debug only
 # export LD_LIBRARY_PATH=/home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main:$LD_LIBRARY_PATH
 # /home/patwang/_gitClone/project_LEXIPON/Bloon_Client/build-hero-Desktop_Qt_5_9_1_GCC_64bit-Debug/src/main/Bloon 2> /dev/null &
 
 # TODO use random port
-./bloond 52811 &
+$BLOON_CLI_HOME/bloond 52811 &
 
 sleep 1
